3fd21fbca361ef59024f164c1dac91f1baedbc9c
Christopher Adams <christopher@fabricatorz.com>
2017-02-22 00:49:28 +0800

    Test authorize URL and fetch access token

--- lib/flickrex/oauth_mock.ex
+++ lib/flickrex/oauth_mock.ex
@@ -1,8 +1,13 @@
 defmodule Flickrex.OAuth.Mock do
   @moduledoc false
 
-  def request(:get, _url, _params, _consumer_key, _consumer_secret, _access_token, _access_token_secret) do
-    body = "oauth_callback_confirmed=true&oauth_token=OAUTH_TOKEN&oauth_token_secret=OAUTH_TOKEN_SECRET"
+  def request(:get, "https://api.flickr.com/services/oauth/request_token", _params, _consumer_key, _consumer_secret, _access_token, _access_token_secret) do
+    body = "oauth_callback_confirmed=true&oauth_token=REQUEST_TOKEN&oauth_token_secret=REQUEST_TOKEN_SECRET"
+    {:ok, {nil, nil, body}}
+  end
+
+  def request(:get, "https://api.flickr.com/services/oauth/access_token", _params, _consumer_key, _consumer_secret, _access_token, _access_token_secret) do
+    body = "fullname=FULLNAME&oauth_token=ACCESS_TOKEN&oauth_token_secret=ACCESS_TOKEN_SECRET&user_nsid=USER_NSID&username=USERNAME"
     {:ok, {nil, nil, body}}
   end
 end
--- test/flickrex/api/auth_test.exs
+++ test/flickrex/api/auth_test.exs
@@ -2,11 +2,32 @@ defmodule Flickrex.API.AuthTest do
   use ExUnit.Case
 
   alias Flickrex.API.Auth
+  alias Flickrex.RequestToken
 
   test "get request token" do
     flickrex = Flickrex.new
     token = Auth.get_request_token(flickrex)
     assert Map.has_key?(token, :oauth_token)
-    assert token.oauth_token == "OAUTH_TOKEN"
+    assert token.oauth_token == "REQUEST_TOKEN"
+  end
+
+  test "get authorize url" do
+    request_token = %RequestToken{oauth_token: "OAUTH_TOKEN"}
+    auth_url = Auth.get_authorize_url(request_token)
+    assert auth_url == "https://api.flickr.com/services/oauth/authorize?oauth_token=OAUTH_TOKEN"
+  end
+
+  test "get authorize url with perms" do
+    request_token = %RequestToken{oauth_token: "OAUTH_TOKEN"}
+    auth_url = Auth.get_authorize_url(request_token, perms: "delete")
+    assert auth_url == "https://api.flickr.com/services/oauth/authorize?oauth_token=OAUTH_TOKEN&perms=delete"
+  end
+
+  test "fetch access token" do
+    request_token = %RequestToken{oauth_token: "OAUTH_TOKEN"}
+    verifier = "VERIFIER"
+    flickrex = Flickrex.new |> Auth.fetch_access_token(request_token, verifier)
+    assert Map.has_key?(flickrex, :access_token)
+    assert flickrex.access_token == "ACCESS_TOKEN"
   end
 end

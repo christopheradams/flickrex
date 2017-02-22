9f33b314cdaf9e85be454148ff6155f367b4b6a0
Christopher Adams <christopher@fabricatorz.com>
2017-02-22 21:13:52 +0800

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,12 +1,23 @@
 defmodule Flickrex.Mixfile do
   use Mix.Project
 
+  @project_description """
+  Flickr API client library for Elixir
+  """
+
+  @version "0.1.0"
+  @source_url "https://github.com/christopheradams/flickrex"
+
   def project do
     [app: :flickrex,
-     version: "0.1.0",
-     elixir: "~> 1.4",
+     version: @version,
+     elixir: "~> 1.3 or ~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: docs(),
+     description: @project_description,
+     source_url: @source_url,
+     package: package(),
      deps: deps()]
   end
 
@@ -20,5 +31,26 @@ defmodule Flickrex.Mixfile do
      {:dialyxir, "~> 0.4", only: [:dev], runtime: false},
      {:credo, "~> 0.5", only: [:dev, :test], runtime: false},
      {:poison, "~> 3.0"}]
+  end
+
+  defp docs() do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: [
+        "README.md": [title: "README"]
+      ]
+    ]
+  end
+
+  defp package do
+    [
+     name: :flickrex,
+     maintainers: ["Christopher Adams"],
+     licenses: ["MIT"],
+     links: %{
+       "GitHub" => @source_url
+     }
+    ]
   end
 end

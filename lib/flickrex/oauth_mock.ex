60cbdc3ec6e758879eecdfe46f59068915e03b39
Christopher Adams <christopher@fabricatorz.com>
2017-02-22 13:28:21 +0800

    Ignore unused args

--- lib/flickrex/oauth_mock.ex
+++ lib/flickrex/oauth_mock.ex
@@ -1,12 +1,12 @@
 defmodule Flickrex.OAuth.Mock do
   @moduledoc false
 
-  def request(:get, "https://api.flickr.com/services/oauth/request_token", _params, _consumer_key, _consumer_secret, _access_token, _access_token_secret) do
+  def request(:get, "https://api.flickr.com/services/oauth/request_token", _, _, _, _, _) do
     body = "oauth_callback_confirmed=true&oauth_token=REQUEST_TOKEN&oauth_token_secret=REQUEST_TOKEN_SECRET"
     {:ok, {nil, nil, body}}
   end
 
-  def request(:get, "https://api.flickr.com/services/oauth/access_token", _params, _consumer_key, _consumer_secret, _access_token, _access_token_secret) do
+  def request(:get, "https://api.flickr.com/services/oauth/access_token", _, _, _, _, _) do
     body = "fullname=FULLNAME&oauth_token=ACCESS_TOKEN&oauth_token_secret=ACCESS_TOKEN_SECRET&user_nsid=USER_NSID&username=USERNAME"
     {:ok, {nil, nil, body}}
   end

105bc2bab744beeef5910d9656e30eacf0f42a2c
Christopher Adams <christopher@fabricatorz.com>
2017-08-06 19:26:55 +0800

    Update http client typespecs

--- lib/flickrex/request/http_client.ex
+++ lib/flickrex/request/http_client.ex
@@ -4,7 +4,12 @@ defmodule Flickrex.Request.HttpClient do
   """
 
   @type http_method :: :get | :post | :put | :delete
-  @callback request(method :: http_method, url :: binary, req_body :: binary, headers :: [{binary, binary}, ...], http_opts :: term) ::
-    {:ok, %{status_code: pos_integer, body: binary}} |
-    {:error, %{reason: any}}
+  @type http_headers :: [{binary, binary}, ...]
+  @type http_status :: pos_integer
+  @type http_response :: %{status_code: pos_integer, headers: http_headers, body: binary}
+  @type success_t :: {:ok, http_response}
+  @type error_t :: {:error, %{reason: any}}
+
+  @callback request(method :: http_method, url :: binary, req_body :: binary, headers :: http_headers, http_opts :: term) ::
+    success_t | error_t
 end

c19e5d399251abfaabc4134183a470a5f6b93527
Christopher Adams <christopher@fabricatorz.com>
2017-08-06 15:56:54 +0800

    Parse Auth status code errors

--- lib/flickrex/parsers/auth.ex
+++ lib/flickrex/parsers/auth.ex
@@ -1,7 +1,7 @@
 defmodule Flickrex.Parsers.Auth do
   @moduledoc false
 
-  def parse_request_token({:ok, %{body: body} = resp}) do
+  def parse_request_token({:ok, %{status_code: 200, body: body} = resp}) do
     oauth_token = URI.decode_query(body, %{})
 
     token = oauth_token["oauth_token"]
@@ -23,9 +23,11 @@ defmodule Flickrex.Parsers.Auth do
     {:ok, %{resp | body: parsed_body}}
   end
 
-  def parse_request_token(val), do: val
+  def parse_request_token(val) do
+    parse_token(val)
+  end
 
-  def parse_access_token({:ok, %{body: body} = resp}) do
+  def parse_access_token({:ok, %{status_code: 200, body: body} = resp}) do
     access_token = URI.decode_query(body, %{})
 
     parsed_body = %{
@@ -39,5 +41,17 @@ defmodule Flickrex.Parsers.Auth do
     {:ok, %{resp | body: parsed_body}}
   end
 
-  def parse_access_token(val), do: val
+  def parse_access_token(val) do
+    parse_token(val)
+  end
+
+  defp parse_token({:ok, %{status_code: 401, body: body} = resp}) do
+    {:error, %{resp | body: URI.decode_query(body)}}
+  end
+
+  defp parse_token({:ok, %{status_code: status_code} = resp}) when status_code >= 400 do
+    {:error, resp}
+  end
+
+  defp parse_token(val), do: val
 end
--- test/flickrex/parsers/auth_test.exs
+++ test/flickrex/parsers/auth_test.exs
@@ -3,6 +3,9 @@ defmodule Flickrex.Parsers.AuthTest do
 
   alias Flickrex.Parsers
 
+  @text_headers [{"content-type", "text/plain; charset=utf-8"}]
+  @html_headers [{"content-type", "text/html; charset=utf-8"}]
+
   test "parse_request_token/1 parses a request token" do
     body = "oauth_callback_confirmed=true&oauth_token=TOKEN&oauth_token_secret=TOKEN_SECRET"
 
@@ -19,10 +22,24 @@ defmodule Flickrex.Parsers.AuthTest do
     }
   end
 
-  test "parse_request_token/1 passes through responses with no body" do
-    response = {:ok, %{status_code: 400, headers: []}}
+  test "parse_request_token/1 parses auth errors" do
+    body = "oauth_problem=consumer_key_unknown"
 
-    assert Parsers.Auth.parse_request_token(response) == response
+    response = {:ok, %{status_code: 401, headers: @text_headers, body: body}}
+
+    parsed_response = Parsers.Auth.parse_request_token(response)
+
+    assert parsed_response ==
+    {:error, %{body: %{"oauth_problem" => "consumer_key_unknown"},
+               headers: [{"content-type", "text/plain; charset=utf-8"}],
+               status_code: 401}}
+  end
+
+  test "parse_request_token/1 returns other errors" do
+    resp = %{status_code: 404, headers: @html_headers, body: ""}
+    parsed_response = Parsers.Auth.parse_request_token({:ok, resp})
+
+    assert {:error, resp} == parsed_response
   end
 
   test "parse_request_token/1 passes errors through" do
@@ -47,6 +64,26 @@ defmodule Flickrex.Parsers.AuthTest do
       user_nsid: "NSID",
       username: "USERNAME"
     }
+  end
+
+  test "parse_access_token/1 parses auth errors" do
+    body = "oauth_problem=token_rejected"
+
+    response = {:ok, %{status_code: 401, headers: @text_headers, body: body}}
+
+    parsed_response = Parsers.Auth.parse_access_token(response)
+
+    assert parsed_response ==
+    {:error, %{body: %{"oauth_problem" => "token_rejected"},
+               headers: [{"content-type", "text/plain; charset=utf-8"}],
+               status_code: 401}}
+  end
+
+  test "parse_access_token/1 returns other errors" do
+    resp = %{status_code: 404, headers: @html_headers, body: ""}
+    parsed_response = Parsers.Auth.parse_access_token({:ok, resp})
+
+    assert {:error, resp} == parsed_response
   end
 end
 

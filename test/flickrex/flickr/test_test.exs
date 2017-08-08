16a0842fe1064984bc99b3c52f028d6a2f01804f
Christopher Adams <christopher@fabricatorz.com>
2017-08-08 11:29:47 +0800

    Test echo method against API

---
+++ test/flickrex/flickr/test_test.exs
@@ -0,0 +1,20 @@
+defmodule Flickrex.Flickr.TestTest do
+  use ExUnit.Case
+
+  @moduletag :flickr_api
+
+  test "test echo/1" do
+    {:ok, resp} =
+      [test: "test"]
+      |> Flickrex.Flickr.Test.echo()
+      |> Flickrex.request()
+
+    assert %{status_code: 200, headers: _headers, body: body} = resp
+
+    assert %{"format" => %{"_content" => "json"},
+             "method" => %{"_content" => "flickr.test.echo"},
+             "nojsoncallback" => %{"_content" => "1"},
+             "stat" => "ok",
+             "test" => %{"_content" => "test"}} = body
+  end
+end

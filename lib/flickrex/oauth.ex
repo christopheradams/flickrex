6012e5161a31c036c29f403563bd322f4d7f27c8
Christopher Adams <christopher@fabricatorz.com>
2017-08-22 17:50:41 +0800

    Redo types

--- lib/flickrex/oauth.ex
+++ lib/flickrex/oauth.ex
@@ -1,13 +1,16 @@
 defmodule Flickrex.OAuth do
   @moduledoc false
 
+  @type method :: String.t
+  @type url :: String.t
+  @type params :: Keyword.t
   @type consumer_key :: String.t
   @type consumer_secret :: String.t
   @type token :: nil | String.t
   @type token_secret :: nil | String.t
-  @type signed_params :: [{String.t, String.Chars.t}]
+  @type signed :: [{String.t, String.Chars.t}]
 
-  @spec sign(binary, binary, Keyword.t, consumer_key, consumer_secret, token, token_secret) :: signed_params
+  @spec sign(method, url, params, consumer_key, consumer_secret, token, token_secret) :: signed
   def sign(method, url, params, consumer_key, consumer_secret, token, token_secret) do
     credentials = OAuther.credentials(
       consumer_key: consumer_key,

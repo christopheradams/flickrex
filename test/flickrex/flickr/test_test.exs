02a87c24fa0ef8c4769dddd205fabd932342846b
Christopher Adams <christopher@fabricatorz.com>
2018-02-18 12:55:51 +0800

    Test 404 response

--- test/flickrex/flickr/test_test.exs
+++ test/flickrex/flickr/test_test.exs
@@ -12,6 +12,22 @@ defmodule Flickrex.Flickr.TestTest do
     test_format(:json, %{"_content" => "json"}, "application/json")
   end
 
+  test "test 404" do
+    {:error, resp} =
+      [test: "test"]
+      |> Flickrex.Flickr.Test.echo()
+      |> Map.put(:parser, &Flickrex.Parsers.Rest.parse/2)
+      |> Map.put(:path, "services/test")
+      |> Flickrex.request()
+
+    assert %{status_code: 404, headers: headers, body: _body} = resp
+
+    new_headers = :hackney_headers_new.new(headers)
+    content_type = :hackney_headers_new.get_value("content-type", new_headers)
+
+    assert content_type == "text/html; charset=UTF-8"
+  end
+
   def test_format(req_format, resp_format_content, resp_content_type) do
     {:ok, resp} =
       [test: "test"]

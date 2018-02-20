3db232b10832e6ef1d5e9cc686a3b010b4c32104
Christopher Adams <christopher@fabricatorz.com>
2018-02-20 18:21:16 +0800

    Document operation protocol

--- lib/flickrex/operation.ex
+++ lib/flickrex/operation.ex
@@ -1,4 +1,28 @@
 defprotocol Flickrex.Operation do
+  @moduledoc """
+  An operation to perform on Flickr
+
+  This module defines a protocol for executing operations via the Flickr
+  API. The modules implementing the protocol handle one service, including
+  authentication, uploads, and "Rest" methods:
+
+  - `Flickrex.Operation.Auth.AccessToken`
+  - `Flickrex.Operation.Auth.RequestToken`
+  - `Flickrex.Operation.Auth.AuthorizeUrl`
+  - `Flickrex.Operation.Rest`
+  - `Flickrex.Operation.Upload`
+
+  The operations are created with the following modules:
+
+  - `Flickrex.Auth`
+  - `Flickrex.Rest`
+  - `Flickrex.Upload`
+  - `Flickrex.Flickr`
+
+  The functions in those modules create a data structure that implements this protocol,
+  when you can then call `perform/2` on it.
+  """
+
   alias Flickrex.Config
 
   @spec perform(t, Config.t()) :: term

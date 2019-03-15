fd140dec50930f762877ea70ff79ffbf6d20d6b6
Christopher Adams <christopher@fabricatorz.com>
2019-03-15 19:24:16 +0800

    Format config file

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,7 @@
 use Mix.Config
 
-config :flickrex, :config, [
+config :flickrex, :config,
   consumer_key: System.get_env("FLICKR_CONSUMER_KEY"),
   consumer_secret: System.get_env("FLICKR_CONSUMER_SECRET"),
   oauth_token: System.get_env("FLICKR_ACCESS_TOKEN"),
-  oauth_token_secret: System.get_env("FLICKR_ACCESS_SECRET"),
-]
+  oauth_token_secret: System.get_env("FLICKR_ACCESS_SECRET")

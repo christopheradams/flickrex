649a3fcbb5cd96a1284dc0e5b37b9005d53984f9
Christopher Adams <christopher@cxadams.com>
2020-09-18 14:27:58 +0800

    Set minimum version to elixir 1.9

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Flickrex.Mixfile do
     [
       app: :flickrex,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
